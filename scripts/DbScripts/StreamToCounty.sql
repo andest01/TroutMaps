8c6d31b303845d21af9ad4611e5e476c394eaecf
standersnerdery <standers@nerdery.com>
2014-05-26 23:46:23 -0600

    lotsa new stuff

---
+++ scripts/DbScripts/StreamToCounty.sql
@@ -0,0 +1,4 @@
+﻿SELECT county.*, stream.kittle_nam, stream.gid from public."Minnesota_County_Borders" county, 
+public."minnesota_dnr_troutstreamsfiltered" stream
+where ST_Intersects(county.geom, stream.geom)
+order by stream.gid
